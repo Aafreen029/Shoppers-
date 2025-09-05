32fdb13b6be56e7b61452050225da5d786bfc59a
Aafreen Zehra <zaafreen029@gmail.com>
2025-09-05 18:16:25 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -156,5 +156,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "anand.augurstech@gmail.com"
+EMAIL_HOST_USER = "*********"
 EMAIL_HOST_PASSWORD = "fmnpqsfufdbdkqwu"
